7f19b433fdf529cf1050c320a373c53fb5e97444
khamza <khamza.khudayberdiyev@gmail.com>
2023-06-08 15:19:44 +0500

    init

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button class="btn btn-primary">OK</button>
+    <button class="btn btn-danger">OK</button>
+    <button class="btn btn-info">OK</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
