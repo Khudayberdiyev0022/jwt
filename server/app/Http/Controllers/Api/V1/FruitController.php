ff8d8bdfc88f9de16946e26c5ba6b79cdc204c56
khamza <khamza.khudayberdiyev@gmail.com>
2023-06-12 08:52:46 +0500

    updated

--- server/app/Http/Controllers/Api/V1/FruitController.php
+++ server/app/Http/Controllers/Api/V1/FruitController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Models\Fruit;
 use Illuminate\Http\Request;
 
 class FruitController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
       $fruits = Fruit::all();
@@ -16,22 +19,47 @@ class FruitController extends Controller
       return response()->json(['data' => $fruits], 200);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-
+        //
     }
 
-    public function show(Fruit $fruit)
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
     {
         //
     }
 
-    public function update(Request $request, Fruit $fruit)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
     {
         //
     }
 
-    public function destroy(Fruit $fruit)
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
     {
         //
     }
